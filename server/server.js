63e7ff818a16342b7ff68dd21fe60920b7bc3b6d
Andrii Pereverziev <y4t6official@gmail.com>
2018-11-05 23:41:37 +0200

    server changes

--- server/server.js
+++ server/server.js
@@ -9,11 +9,13 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.DATABASE);
+mongoose.connect(process.env.MONGODB_URI);
 
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use(cookieParser());
+
+app.use(express.static('client/build'));
 
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
@@ -297,6 +299,14 @@ app.get('/api/users/addToCart',auth,(req,res)=>{
         }
     })
 });
+
+// DEFAULT
+if( process.env.NODE_ENV === 'production' ){
+    const path = require('path');
+    app.get('/*',(req,res)=>{
+        res.sendfile(path.resolve(__dirname,'../client','build','index.html'))
+    })
+}
 
 const port = process.env.PORT || 4000;
 
